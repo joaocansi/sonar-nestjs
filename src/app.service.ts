7eb3f7ac463073ec43c15892ea5ea5dd426da2ac
joaocansi <whitshs2003@gmail.com>
2024-11-20 20:34:22 -0300

    feat: test

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,24 @@
 import { Injectable } from '@nestjs/common';
 
+function calculateDiscount(price) {
+  if (price > 100) {
+    return price * 0.9;
+  } else {
+    return price;
+  }
+}
+
+function calculateFinalPrice(price) {
+  if (price > 100) {
+    return price * 0.9; // Duplicate logic
+  } else {
+    return price;
+  }
+}
+
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return calculateDiscount(10) + calculateFinalPrice(10);
   }
 }
