0e3743555e21f7d58404af66ad3968a39f0fa073
joaocansi <whitshs2003@gmail.com>
2024-11-27 00:47:45 -0300

    feat: test

---
+++ terraform/main.tf
@@ -0,0 +1,60 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_security_group" "example" {
+  name_prefix = "example"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami           = "ami-006dcf34c09e50022" // Amazon Linux 2 AMI
+  instance_type = "t2.micro"
+  key_name      = "ec2"
+  vpc_security_group_ids = [
+    aws_security_group.example.id,
+  ]
+  user_data = <<-EOF
+              #!/bin/bash
+              yum install -y docker
+              systemctl enable docker
+              systemctl start docker
+              sudo chown $USER /var/run/docker.sock
+              docker run -p 80:80 -d nginx
+              EOF
+}
+
+resource "aws_eip" "example" {
+  instance = aws_instance.example.id
+}
+
+output "elastic_ip" {
+  value = aws_eip.example.public_ip
+}
