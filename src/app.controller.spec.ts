a27abb1d65fb0cf4342a437949b4b41a1b4a297c
joaocansi <whitshs2003@gmail.com>
2024-11-27 21:29:01 -0300

    test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -16,7 +16,9 @@ describe('AppController', () => {
 
   describe('root', () => {
     it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe(20);
+      expect(appController.getHello()).toEqual({
+        hello: 'world',
+      });
     });
   });
 });
