7f0674ac7b9902627566f5610af9ec1d2ce9c9f6
joaocansi <whitshs2003@gmail.com>
2024-11-27 23:03:28 -0300

    test

--- terraform/setup.sh
+++ terraform/setup.sh
@@ -4,4 +4,4 @@ systemctl enable docker
 systemctl start docker
 sudo chown $USER /var/run/docker.sock
 docker ps -q --filter "ancestor=joaocansi/nestjs-app" | xargs -r docker stop | xargs -r docker rm
-docker run -p 80:3000 -d joaocansi/nestjs-app:$COMMIT
+docker run -p 80:3000 -d joaocansi/nestjs-app
